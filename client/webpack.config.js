c5ab41b4c75f9404468e16399d70ff58e887f23a
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-19 21:07:36 +0100

    Add configs for compiling and hot reloading With this commit the app compiles successfully and renders in browser

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+};
